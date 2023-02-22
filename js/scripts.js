d07d95e198ad2ac7a15a1fbbfd4ba065d3784d4e
Daniel Li <danielcli-dev@gmail.com>
2023-02-22 15:53:00 -0800

    Added list with Pokemon names and heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
+// Sets the data in array
 let pokemonList = [
-  { name: Bulbasaur, height: 7, types: ["grass", "poison"] },
-  { name: Charmander, height: 6, types: ["fire"] },
-  { name: Squirtle, height: 5, types: ["water"] },
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Charmander", height: 1.7, types: ["fire"] },
+  { name: "Squirtle", height: 1, types: ["water"] },
 ];
+
+// for (i = 0; i < pokemonList.length; i++) {
+//   document.write(
+//     `${pokemonList[i].name} (height: ${pokemonList[i].height}) ${
+//       pokemonList[i].height > 1 ? "- Wow, that's big!" : ""
+//     } <br/><br/>`
+//   );
+// }
+
+for (i = 0; i < pokemonList.length; i++) {
+  let name = pokemonList[i].name;
+  let height = pokemonList[i].height;
+
+  document.write(` <li>
+  ${name} (height: ${pokemonList[i].height}) ${
+    pokemonList[i].height > 1 ? "-Wow, that's big!" : ""
+  }
+  </li>`);
+}
