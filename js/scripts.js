0a5550288002ff4081692d2ece0d0082f45287b3
Daniel Li <danielcli-dev@gmail.com>
2023-02-27 16:51:43 -0800

    Wrapped pokemonList array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
-// Sets the pokemon objects in array
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Charmander", height: 1.7, types: ["fire"] },
-  { name: "Squirtle", height: 1, types: ["water"] },
-];
-let pokemonList2 = [
-  { name: "Caterpie", height: 0.3, types: ["bug"] },
-  { name: "Weedle", height: 0.3, types: ["bug", "poison"] },
-  { name: "Pidgey", height: 0.3, types: ["flying", "normal"] },
-];
+// Moved array into an IIFE with added functions
+
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Charmander", height: 1.7, types: ["fire"] },
+    { name: "Squirtle", height: 1, types: ["water"] },
+    { name: "Caterpie", height: 0.3, types: ["bug"] },
+    { name: "Weedle", height: 0.3, types: ["bug", "poison"] },
+    { name: "Pidgey", height: 0.3, types: ["flying", "normal"] },
+  ];
+
+  function add(pokemon) {
+    if (typeof pokemon === "object") pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 
 // For loop to print name and height from pokemonList array
 
@@ -24,15 +38,18 @@ function divide(dividend, divisor) {
 // Function for printing array values
 function printArrayDetails(user) {
   document.write(` <li>
-    ${user.name} (height: ${user.height}) ${
-    user.height > 1 ? "- Wow, that's big!" : ""
-  }
+    ${user.name} ${
+    typeof user.height !== "undefined" ? `(height: ${user.height})` : ""}
+    
+   ${user.height > 1 ? "- Wow, that's big!" : ""}
     </li>`);
 }
 
 // Replaced the for loop with forEach() function
-pokemonList.forEach(printArrayDetails);
-pokemonList2.forEach(printArrayDetails);
+// pokemonRepository.getAll().forEach(printArrayDetails);
+
+pokemonRepository.add({ name: "Pikachu" });
+pokemonRepository.getAll().forEach(printArrayDetails);
 
 console.log(divide(4, 2));
 console.log(divide(7, 0));
